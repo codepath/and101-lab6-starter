e748a7cae486518afaf28a51252540fcc40ec100
espertus <espertus@users.noreply.github.com>
2025-07-09 20:54:46 -0700

    Update for 2025

--- app/src/main/java/com/driuft/random_pets_starter/MainActivity.kt
+++ app/src/main/java/com/driuft/random_pets_starter/MainActivity.kt
@@ -1,30 +1,49 @@
 package com.driuft.random_pets_starter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
-        getDogImageURL()
+        val button = findViewById<Button>(R.id.petButton)
+        setupButton(button)
+    }
+
+    private fun setupButton(button: Button) {
+        button.setOnClickListener {
+            getDogImageURL()
+        }
     }
 
     private fun getDogImageURL() {
         val client = AsyncHttpClient()
-
+        val imageView = findViewById<ImageView>(R.id.petView)
         client["https://dog.ceo/api/breeds/image/random", object : JsonHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
-                Log.d("Dog Success", "$json")
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                val petImageURL = json.jsonObject.getString("message")
+                Glide.with(this@MainActivity)
+                    .load(petImageURL)
+                    .fitCenter()
+                    .into(imageView)
             }
 
             override fun onFailure(
@@ -33,8 +52,7 @@ class MainActivity : AppCompatActivity() {
                 errorResponse: String,
                 throwable: Throwable?
             ) {
-                Log.d("Dog Error", errorResponse)
-            }
+                Log.d("Dog Error", errorResponse)            }
         }]
     }
 }
